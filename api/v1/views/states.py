17af5db161cf859c9a696b31e091e2f7c418a023
hany0ageeb <hany0ageeb@gmail.com>
2024-03-30 05:17:13 -0700

    Changes states.py to meet requiremnets!

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,9 +4,11 @@ module - states
 """
 from api.v1.views import app_views
 from flask import jsonify, abort, request
-from werkzeug.exceptions import BadRequest
 from models import storage
 from models.state import State
+
+
+__banned_attributes = ('id', 'created_at', 'updated_at')
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -39,7 +41,7 @@ def delete_state(state_id):
     """
     state = storage.get(State, state_id)
     if state:
-        state.delete()
+        storage.delete(state)
         storage.save()
         return jsonify({}), 200
     else:
@@ -52,17 +54,15 @@ def delete_state(state_id):
         strict_slashes=False)
 def create_state():
     """Creates a State: POST /api/v1/states"""
-    try:
-        json_data = request.get_json()
-        if 'name' not in json_data.keys():
-            return jsonify({'error': 'Missing name'}), 400
-        # json_data = {key: value for key, value in json_data.items() if key == 'name'}
-        state = State(**json_data)
-        storage.new(state)
-        storage.save()
-        return jsonify(state.to_dict()), 201
-    except BadRequest:
-        return jsonify({'error': 'Not a JSON'}), 400
+    json_data = request.get_json()
+    if not json_data:
+        abort(400, 'Not a JSON')
+    if 'name' not in json_data.keys():
+        abort(400, 'Missing name')
+    state = State(**json_data)
+    storage.new(state)
+    storage.save()
+    return jsonify(state.to_dict()), 201
 
 
 @app_views.route(
@@ -73,17 +73,15 @@ def update_state(state_id):
     """
     Updates a State object: PUT /api/v1/states/<state_id>
     """
-    banned_attributes = ('id', 'created_at', 'updated_at')
-    try:
-        state = storage.get(State, state_id)
-        if state is None:
-            abort(404)
-        else:
-            json_data = request.get_json()
-            for key, value in json_data.items():
-                if key not in banned_attributes:
-                    setattr(state, key, value)
-            storage.save()
-            return jsonify(state.to_dict()), 200
-    except BadRequest:
-        return jsonify({'error': 'Not a JSON'}), 400
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    else:
+        json_data = request.get_json()
+        if not json_data:
+            abort(400, 'Not a JSON')
+        for key, value in json_data.items():
+            if key not in __banned_attributes:
+                setattr(state, key, value)
+        storage.save()
+        return jsonify(state.to_dict()), 200
